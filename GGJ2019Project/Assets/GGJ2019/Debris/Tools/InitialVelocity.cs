daf50b422a0245b11179fd3d5bd5ee61ff697f1a
Steff Kempink <mswf@live.nl>
2019-01-26 20:36:15 +0100

    Added example level

---
+++ GGJ2019Project/Assets/GGJ2019/Debris/Tools/InitialVelocity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class InitialVelocity : MonoBehaviour
+{
+	private Rigidbody2D rigidBody;
+
+	public Vector2 direction;
+	[Range(0f, 100f)]
+	public float force = 10f;
+
+	[Range(0f, 1f)]
+	public float randomFactor = 0.1f;
+	protected void Awake()
+	{
+		rigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	protected void Start()
+	{
+		var normalizedDir = direction.normalized;
+
+
+		rigidBody.AddForce(normalizedDir * force * Random.Range(1f-randomFactor, 1f), ForceMode2D.Impulse);
+	}
+}
