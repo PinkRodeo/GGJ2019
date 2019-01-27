6b8fda0ca9b1e43c98850f3f553ea74cf3a17354
dimin69 <dimin69gaming@outlook.com>
2019-01-27 12:48:27 +0100

    no message

--- GGJ2019Project/Assets/GGJ2019/Scripts/DialogOnAwakeStarter.cs
+++ GGJ2019Project/Assets/GGJ2019/Scripts/DialogOnAwakeStarter.cs
@@ -5,19 +5,41 @@ using UnityEngine;
 public class DialogOnAwakeStarter : MonoBehaviour
 {
     [SerializeField] DialogScriptableObject EventChain;
+    [SerializeField] bool DoOnce = true;
+
+    bool DoneOnce = false;
 
     [ContextMenu("AddItems")]
     public void Add()
     {
+        if (DoOnce == true && DoOnce == DoneOnce)
+            return;
+
+
+        if (DoOnce == true)
+        {
+            JunkerGameMode.FinishedStoryPart(EventChain.name);
+
+            DoneOnce = true;
+        }
+
+
+
         if (EventChain != null)
             foreach (GameEvent item in EventChain.eventChain)
             {
                 EventManager.AddEvent(item);
             }
+        if (DoOnce == true)
+        {
+            var clawTarget = GetComponent<ClawTarget>();
+            clawTarget.SetLightsVisible(DoOnce != DoneOnce || DoOnce == false);
+        }
     }
 
     private void Awake()
     {
-        Add();
+        if (!JunkerGameMode.HasDoneStoryPart(EventChain.name))
+            Add();
     }
 }
