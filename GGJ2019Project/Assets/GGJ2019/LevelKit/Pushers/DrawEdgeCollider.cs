89549c9c0fe4b928c97b06c0fa65ec5f8e2c173c
Steff Kempink <mswf@live.nl>
2019-01-27 13:47:37 +0100

    Added tutorial, packaged sprites

--- GGJ2019Project/Assets/GGJ2019/LevelKit/Pushers/DrawEdgeCollider.cs
+++ GGJ2019Project/Assets/GGJ2019/LevelKit/Pushers/DrawEdgeCollider.cs
@@ -17,15 +17,18 @@ public class DrawEdgeCollider : MonoBehaviour
 	{
 		this.enabled = false;
 	}
-
+	///*
 	// Update is called once per frame
 	void Update()
     {
+		var trans = this.transform;
+
 		var radius = edgeCollider2D.edgeRadius;
 		var points = edgeCollider2D.points;
 		for (int i = 0; i < edgeCollider2D.pointCount-1; i++)
 		{
-			DebugExtension.DebugCapsule(points[i], points[i + 1], Color.green, radius, 0f, false);
+			DebugExtension.DebugCapsule(trans.TransformPoint(points[i]), trans.TransformPoint(points[i + 1]), Color.green, radius, 0f, false);
 		}
     }
+	//*/
 }
